b1160027118bf569011acebb53581e3117345f44
zubenkoalex <131799752+zubenkoalex@users.noreply.github.com>
2026-05-14 14:46:45 +0700

    fix vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Использование относительных путей
+  base: '/', // Использование относительных путей
   build: {
-    outDir: 'dist', // Выходная директория
+    outDir: 'build', // Выходная директория
     assetsDir: 'assets', // Каталог для статических файлов
     emptyOutDir: true, // Очищать выходную директорию перед сборкой
   },
